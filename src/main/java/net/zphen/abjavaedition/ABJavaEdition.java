3ae804d04ef45b4cde1dad7a14297496a20b700c
ZphenLogos <zphen887@gmail.com>
2025-02-01 19:55:14 -0800

    Initial commit

---
+++ src/main/java/net/zphen/abjavaedition/ABJavaEdition.java
@@ -0,0 +1,15 @@
+package net.zphen.abjavaedition;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ABJavaEdition implements ModInitializer {
+	public static final String MOD_ID = "ab_java_edition";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/zphen/abjavaedition/ABJavaEditionClient.java
@@ -0,0 +1,10 @@
+package net.zphen.abjavaedition;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ABJavaEditionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
